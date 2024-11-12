1cf9a487baee65e51d78360b630a69a162181a45
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-11-12 22:56:07 +0900

    [Feat/#10] 토스페이먼츠 API 연동 (#11)
    * Chore: tosspayments 패키지 설치
    
    - package.json
    - package-lock.json
    
    * Feat(payment.ts): 결제 요청 처리 API 핸들러
    
    * Feat(page.tsx): 루트 page에 결제 테스트 버튼 추가
    
    * Feat(page.tsx): 결제 완료 페이지
    
    * Remove(payments.ts): api 폴더 구조 변경에 따른 폴더 및 파일 삭제
    
    * Feat(PaymentType.ts): 결제 관련 type 추가
    
    * Feat(payments.ts): tosspayments 통신 API 구현
    
    * Feat(page.tsx): 결제 완료 페이지
    
    * Feat(page.tsx): root 페이지에 결제테스트 버튼 추가
    
    * Refactor(page.tsx): vercel build 오류로 인한 awiat 구문 추가
    
    * Refactor(page.tsx): PageProps 없이 searchParams 사용하도록 수정
    
    * Refactor(page.tsx): PageProps 제거 및 searchParams 직접 받도록 수정
    
    * Refactor(page.tsx): searchParams promise 구문으로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,38 @@
+"use client";
+
 import LocationSelector from "@/components/LocationSelector";
 import SearchBar from "@/components/SearchBar";
 import PopularCategories from "@/components/PopularCategories";
 import ProductList from "@/components/ProductList";
+import { loadTossPayments } from "@tosspayments/payment-sdk";
 
-const HomePage = () => {
+export default function Page() {
+  const handleClick = async () => {
+    const clientKey = process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY;
+
+    if (!clientKey) {
+      console.error("Client key is missing");
+      return;
+    }
+
+    const tossPayments = await loadTossPayments(clientKey);
+
+    await tossPayments.requestPayment("카드", {
+      amount: 5000,
+      orderId: Math.random().toString(36).slice(2),
+      orderName: "치킨",
+      successUrl: `${window.location.origin}/api/payments`,
+      failUrl: `${window.location.origin}/api/payments/fail`,
+    });
+  };
+
   return (
     <div className="px-4 sm:px-6 lg:px-8">
       <LocationSelector />
       <SearchBar />
       <PopularCategories />
       <ProductList />
+      <button onClick={handleClick}>치킨 5000원</button>
     </div>
   );
-};
-
-export default HomePage;
+}
