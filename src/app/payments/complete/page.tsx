b0efc0b502dec2373a5c4e9602e85a9a0e5e1b93
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-11-12 22:24:30 +0900

    Feat(page.tsx): 결제 완료 페이지

--- src/app/payments/complete/page.tsx
+++ src/app/payments/complete/page.tsx
@@ -1,6 +1,4 @@
-interface SearchParams {
-  orderId: string | undefined;
-}
+import { PaymentResponse, SearchParams } from "@/types/PaymentType";
 
 export default async function Page({
   searchParams,
@@ -10,12 +8,8 @@ export default async function Page({
   const secretKey = process.env.TOSS_SECRET_KEY || "";
   const basicToken = Buffer.from(`${secretKey}:`, "utf-8").toString("base64");
 
-  if (!searchParams.orderId) {
-    return <div>주문번호가 없습니다.</div>;
-  }
-
-  const url = `https://api.tosspayments.com/v1/payments/${searchParams.orderId}`;
-  const payments = await fetch(url, {
+  const url = `https://api.tosspayments.com/v1/payments/orders/${searchParams.orderId}`;
+  const payments: PaymentResponse = await fetch(url, {
     headers: {
       Authorization: `Basic ${basicToken}`,
       "Content-Type": "application/json",
@@ -23,18 +17,17 @@ export default async function Page({
   }).then((res) => res.json());
 
   const { card } = payments;
-
   return (
     <div>
       <h1>결제가 완료되었습니다</h1>
       <ul>
-        <li>결제 상품 {payments.orderName}</li>
-        <li>주문번호 {payments.orderId}</li>
-        <li>카드회사 {card.company}</li>
-        <li>카드번호 {card.number}</li>
-        <li>결제금액 {card.amount}</li>
+        <li>결제 상품: {payments.orderName}</li>
+        <li>주문번호: {payments.orderId}</li>
+        <li>카드회사: {card?.company || "-"}</li>
+        <li>카드번호: {card?.number || ""}</li>
+        <li>결제금액: {card?.amount || ""}</li>
         <li>
-          결제승인날짜{" "}
+          결제승인날짜:{" "}
           {Intl.DateTimeFormat().format(new Date(payments.approvedAt))}
         </li>
       </ul>
