d6ceb1da19807d4f45872f28e88761aad4c962fb
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-11-19 10:57:36 +0900

    Refactor(payments.ts): next 자체 API 삭제에 따른 불필요한 파일 임시 주석

--- src/pages/api/payments.ts
+++ src/pages/api/payments.ts
@@ -1,44 +1,44 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { PaymentRequest } from "@/types/PaymentType";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { PaymentRequest } from "@/types/PaymentType";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "POST") {
-    res.status(405).json({ error: "Method not allowed" });
-    return;
-  }
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method !== "POST") {
+//     res.status(405).json({ error: "Method not allowed" });
+//     return;
+//   }
 
-  const { orderId, paymentKey, amount } = req.body as PaymentRequest;
-  const secretKey = process.env.TOSS_SECRET_KEY || "";
+//   const { orderId, paymentKey, amount } = req.body as PaymentRequest;
+//   const secretKey = process.env.TOSS_SECRET_KEY || "";
 
-  if (!orderId || !paymentKey || !amount) {
-    res.status(400).json({ error: "Missing required fields" });
-    return;
-  }
+//   if (!orderId || !paymentKey || !amount) {
+//     res.status(400).json({ error: "Missing required fields" });
+//     return;
+//   }
 
-  const url = "https://api.tosspayments.com/v1/payments/confirm";
-  const basicToken = Buffer.from(`${secretKey}:`, "utf-8").toString("base64");
+//   const url = "https://api.tosspayments.com/v1/payments/confirm";
+//   const basicToken = Buffer.from(`${secretKey}:`, "utf-8").toString("base64");
 
-  const response = await fetch(url, {
-    method: "POST",
-    body: JSON.stringify({
-      amount,
-      orderId,
-      paymentKey,
-    }),
-    headers: {
-      Authorization: `Basic ${basicToken}`,
-      "Content-Type": "application/json",
-    },
-  });
+//   const response = await fetch(url, {
+//     method: "POST",
+//     body: JSON.stringify({
+//       amount,
+//       orderId,
+//       paymentKey,
+//     }),
+//     headers: {
+//       Authorization: `Basic ${basicToken}`,
+//       "Content-Type": "application/json",
+//     },
+//   });
 
-  if (!response.ok) {
-    const errorResponse = await response.json();
-    res.status(response.status).json(errorResponse);
-    return;
-  }
+//   if (!response.ok) {
+//     const errorResponse = await response.json();
+//     res.status(response.status).json(errorResponse);
+//     return;
+//   }
 
-  res.redirect(`/payments/complete?orderId=${orderId}`);
-}
+//   res.redirect(`/payments/complete?orderId=${orderId}`);
+// }
