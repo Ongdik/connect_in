52402f724c74975d0c8344aa3689d6cc00ef3d2c
Lee Dong Ik <65389677+Ongdik@users.noreply.github.com>
2024-11-19 10:54:26 +0900

    Refactor(page.tsx): 결제 검증 API 전송

--- src/app/payments/complete/page.tsx
+++ src/app/payments/complete/page.tsx
@@ -1,39 +1,49 @@
 import { PaymentResponse, SearchParams } from "@/types/PaymentType";
 
-// SearchParams 인터페이스를 사용하여 searchParams를 직접 받음
 export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<SearchParams>;
 }) {
-  const { orderId } = await searchParams;
+  const { orderId, amount } = await searchParams;
 
-  const secretKey = process.env.TOSS_SECRET_KEY || "";
-  const basicToken = Buffer.from(`${secretKey}:`, "utf-8").toString("base64");
+  if (!orderId || !amount) {
+    return <div>잘못된 요청입니다.</div>;
+  }
 
   const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL; // 환경 변수에서 URL 가져오기
-  const url = `${backendUrl}/api/payments/${orderId}`; // 백엔드 API 호출
-
-  const payments: PaymentResponse = await fetch(url, {
+  const response = await fetch(`${backendUrl}/payments/confirm`, {
+    method: "POST",
     headers: {
-      Authorization: `Basic ${basicToken}`,
       "Content-Type": "application/json",
     },
-  }).then((res) => res.json());
+    body: JSON.stringify({ orderId, amount }),
+  });
 
-  const { card } = payments;
+  if (!response.ok) {
+    return <div>결제 검증에 실패했습니다.</div>;
+  }
+
+  const paymentsResponse: PaymentResponse = await response.json();
+
+  // `result` 객체 추출 및 디버깅
+  const payments = paymentsResponse.result;
+
   return (
     <div>
       <h1>결제가 완료되었습니다</h1>
       <ul>
         <li>결제 상품: {payments.orderName}</li>
         <li>주문번호: {payments.orderId}</li>
-        <li>카드회사: {card?.company || "-"}</li>
+        <li>결제금액: {payments.amount || ""}</li>
+        {/* <li>카드회사: {card?.company || "-"}</li>
         <li>카드번호: {card?.number || ""}</li>
-        <li>결제금액: {card?.amount || ""}</li>
+        <li>결제금액: {card?.amount || ""}</li> */}
         <li>
           결제승인날짜:{" "}
-          {Intl.DateTimeFormat().format(new Date(payments.approvedAt))}
+          {payments.approvedAt
+            ? Intl.DateTimeFormat().format(new Date(payments.approvedAt))
+            : "날짜 정보 없음"}
         </li>
       </ul>
     </div>
